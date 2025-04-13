9bd9738b92f92ba9eb38b3d1ef00a334a3c7f6dc
EveRoscher <evelyncarol@hotmail.es>
2025-04-12 23:25:45 -0300

    Add toast notification when product is added to cart

--- src/components/productItemCard/ProductItemCard.jsx
+++ src/components/productItemCard/ProductItemCard.jsx
@@ -1,4 +1,5 @@
-import { Card, Button, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Button, Toast, ToastContainer } from "react-bootstrap";
 
 const ProductItemCard = ({
   title,
@@ -7,6 +8,14 @@ const ProductItemCard = ({
   stock,
   onCountProductsCart,
 }) => {
+  const [showToast, setShowToast] = useState(false);
+
+  const handleButtonClick = () => {
+    onCountProductsCart();
+
+    setShowToast(true);
+  };
+
   return (
     <>
       <div className="col-md-4">
@@ -17,12 +26,13 @@ const ProductItemCard = ({
             <Card.Subtitle className="mb-2 text-muted text-center">
               {price}
             </Card.Subtitle>
+
             <div className="d-grid gap-2">
               <Button
                 variant={stock > 0 ? "success" : "secondary"}
                 size="lg"
                 disabled={stock <= 0}
-                onClick={onCountProductsCart}
+                onClick={handleButtonClick}
               >
                 {stock > 0 ? "Agregar al carrito" : "Sin stock"}
               </Button>
@@ -30,6 +40,18 @@ const ProductItemCard = ({
           </Card.Body>
         </Card>
       </div>
+
+      <ToastContainer position="top-end" className="p-3">
+        <Toast
+          onClose={() => setShowToast(false)}
+          show={showToast}
+          bg="success"
+          delay={1500}
+          autohide
+        >
+          <Toast.Body className="text-white">Producto agregado</Toast.Body>
+        </Toast>
+      </ToastContainer>
     </>
   );
 };
